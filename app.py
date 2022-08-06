f81d33c838dd8657c37ab94ab1218db19e3e4758
vinayak0000-code <73603869+vinayak0000-code@users.noreply.github.com>
2022-08-06 08:39:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,41 +5,26 @@ import pickle
 
 
 app = Flask(__name__)
-model1 = pickle.load(open('Titanic_modelKNN.pkl','rb')) 
+model = pickle.load(open('lineartitnic.pkl','rb')) 
 
 
 @app.route('/')
 def home():
+      return render_template("index.html")
   
-    return render_template("index.html")
+@app.route('/pr',methods=['GET'])
+def pr():
+  exp = float(request.args.get('exp'))
+  exp1 = float(request.args.get('exp1'))
+  exp2 = float(request.args.get('exp2'))
+  exp3 = float(request.args.get('exp3'))
+  exp4 = float(request.args.get('exp4'))
+  exp5 = float(request.args.get('exp5'))
   
-@app.route('/predict',methods=['GET'])
+  prediction = model.predict([[exp,exp1,exp2,exp3,exp4,exp5]])
 
-def predict():
-    
-    
-    '''
-    For rendering results on HTML GUI
-    '''
-    Pclass = float(request.args.get('Pclass'))
-    Age = float(request.args.get('Age'))
-    SibSp = float(request.args.get('SibSp'))
-    Parch = float(request.args.get('Parch'))
-    Fare = float(request.args.get('Fare'))
-    Sex = float(request.args.get('Sex'))
-    if Sex == 1:
-        Sex = 'male'
-    else:
-        Sex = 'female'
-
-    
-    prediction = model1.predict([[Pclass, Sex, Age, SibSp, Parch, Fare]])
-    if prediction == [1]:
-      prediction_text = 'Survived'
-    else:
-      prediction_text = 'Not Survived'
-        
-    return render_template('index.html', prediction_text='KNN model has predict about the survival of person for given features  : {}'.format(prediction_text))
+  return render_template('index.html', prediction_text='Regression Model  has predicted is : {}'.format(prediction))
 
 
-app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
